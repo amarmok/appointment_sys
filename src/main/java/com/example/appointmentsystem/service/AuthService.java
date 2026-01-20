82b68dba5b66af0d6a561c3f4a2700ee6029f5c0
amarmok <ahmadhatahta775@gmail.com>
2026-01-20 12:33:34 +0300

    auth

--- src/main/java/com/example/appointmentsystem/service/AuthService.java
+++ src/main/java/com/example/appointmentsystem/service/AuthService.java
@@ -20,7 +20,6 @@ public class AuthService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    // تسجيل مستخدم جديد
     public void register(RegisterRequest request) {
         if (userRepository.existsByEmail(request.email())) {
             throw new BusinessException("Email already exists");
@@ -29,27 +28,25 @@ public class AuthService {
         User user = User.builder()
                 .fullName(request.fullName())
                 .email(request.email())
-                .password(passwordEncoder.encode(request.password())) // تشفير كلمة المرور
+                .password(passwordEncoder.encode(request.password())) 
                 .role(request.role())
                 .build();
 
         userRepository.save(user);
     }
 
-    // تسجيل الدخول وتوليد JWT
     public AuthResponseTemp login(LoginRequest request) {
-        // التحقق من البريد وكلمة المرور
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.email(), request.password())
         );
 
-        // جلب المستخدم
+        
         User user = userRepository.findByEmail(request.email())
                 .orElseThrow(() -> new BusinessException("User not found"));
 
-        // توليد JWT
+        
         String token = jwtService.generateToken(user.getEmail());
 
-        return new AuthResponseTemp(token); // إعادة التوكن للمستخدم
+        return new AuthResponseTemp(token); 
     }
 }
